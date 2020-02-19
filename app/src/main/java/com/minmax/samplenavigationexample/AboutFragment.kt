5afaf0d2afd46521084e11abc37c16ac632dc075
Murodjon <murodjon@next-aeon.com>
2020-02-19 17:19:11 +0900

    add popbackstack

--- app/src/main/java/com/minmax/samplenavigationexample/AboutFragment.kt
+++ app/src/main/java/com/minmax/samplenavigationexample/AboutFragment.kt
@@ -6,18 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass.
  */
 class AboutFragment : Fragment() {
 
+    lateinit var backButton: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        backButton = view.findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        return view
     }
 
 
